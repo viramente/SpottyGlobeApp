f837256f38458037be2f4fe7439806ca4d2a13e1
= <viramente@gmail.com>
2024-01-12 00:01:36 +0200

    SpGlApp-01 buttons

--- app/src/main/java/lv/viramente/spottyglobeapp/MainActivity.kt
+++ app/src/main/java/lv/viramente/spottyglobeapp/MainActivity.kt
@@ -2,10 +2,20 @@ package lv.viramente.spottyglobeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import lv.viramente.spottyglobeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
     }
+
 }
